549d6b1ed1c75557ce9b22f6d67a8dcca6c239b9
Hrant P. Hratchian <hhratchian@ucmerced.edu>
2020-05-31 17:36:55 -0700

    Added if/elseif/else/endif checks. Also added debug printing flag to the rules hash.

--- Tests/prgm004.f90
+++ Tests/prgm004.f90
@@ -1,8 +1,8 @@
-      Program Prgm003
+      Program Prgm004
  1000 Format(1x,'This is a test.')
  2000 Format(1x,'i = ',I2)
       write(*,1000)
       do i = 1,10
          write(*,1000) i
       endDo
-      End Program Prgm003
+      End Program Prgm004
---
+++ Tests/prgm006.f90
@@ -0,0 +1,18 @@
+      Program Prgm006
+      implicit none
+      logical::tempLogical
+ 1000 Format(1x,'This is a test.')
+ 2000 Format(1x,'i = ',I2)
+      write(*,1000)
+      do i = 1,10
+        write(*,1000) i
+      endDo
+      tempLogical = .true.
+      if(.true.) then
+        write(*,*)' This is a test of true.'
+        tempLogical = .not.tempLogical
+      else
+        write(*,*)' This is WRONG!'
+      endIf
+      write(*,*)' At the end of the program, tempLogical is ',tempLogical
+      End Program Prgm006
